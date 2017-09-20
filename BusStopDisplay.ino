e249225072dbe4cc1aa2b86d650710b6dfa81c00
Simon Weiler <sweiler@thoughtworks.com>
2017-09-20 23:04:23 +0200

    add digits

--- BusStopDisplay.ino
+++ BusStopDisplay.ino
@@ -1,20 +1,18 @@
 
 byte displayContent[8][32];
-const byte displayLength = 40;
+const byte displayLength = 56;
 
 void setup() {
   initLED();
-  displayContent[6][0] = B00000000;
-  displayContent[5][0] = B00100100;
-  displayContent[4][0] = B01011010;
-  displayContent[3][0] = B01000010;
-  displayContent[2][0] = B00100100;
-  displayContent[1][0] = B00011000;
+  
+
+
+
+  setText("12394567", 8);
 }
 
 
 void loop() {
   updateLED();
-  delay(100);
 }
 
--- LED.ino
+++ LED.ino
@@ -5,6 +5,10 @@ const int slaveSelectPin = 5;
 byte currentOffset;
 unsigned long lastUpdate;
 
+byte digits[10][8];
+
+
+
 void initLED() {
   pinMode(slaveSelectPin, OUTPUT);
   SPI.begin();
@@ -18,7 +22,79 @@ void initLED() {
   clearScreen();
 
   currentOffset = 0;
-  lastUpdate = millis();
+  lastUpdate = 0;
+
+  digits[1][7] = B0000;
+  digits[1][6] = B0001;
+  digits[1][5] = B0011;
+  digits[1][4] = B0101;
+  digits[1][3] = B1001;
+  digits[1][2] = B0001;
+  digits[1][1] = B0001;
+  digits[1][0] = B0000;
+
+  digits[2][7] = B0000;
+  digits[2][6] = B0110;
+  digits[2][5] = B1001;
+  digits[2][4] = B0001;
+  digits[2][3] = B0010;
+  digits[2][2] = B0100;
+  digits[2][1] = B1111;
+  digits[2][0] = B0000;
+
+  digits[3][7] = B0000;
+  digits[3][6] = B1110;
+  digits[3][5] = B0001;
+  digits[3][4] = B0110;
+  digits[3][3] = B0001;
+  digits[3][2] = B0001;
+  digits[3][1] = B1110;
+  digits[3][0] = B0000;
+
+  digits[9][7] = B0000;
+  digits[9][6] = B0110;
+  digits[9][5] = B1001;
+  digits[9][4] = B1001;
+  digits[9][3] = B0111;
+  digits[9][2] = B0001;
+  digits[9][1] = B1110;
+  digits[9][0] = B0000;
+
+  digits[4][7] = B0000;
+  digits[4][6] = B0010;
+  digits[4][5] = B0110;
+  digits[4][4] = B1010;
+  digits[4][3] = B1111;
+  digits[4][2] = B0010;
+  digits[4][1] = B0010;
+  digits[4][0] = B0000;
+
+  digits[5][7] = B0000;
+  digits[5][6] = B1111;
+  digits[5][5] = B1000;
+  digits[5][4] = B1110;
+  digits[5][3] = B0001;
+  digits[5][2] = B0001;
+  digits[5][1] = B1110;
+  digits[5][0] = B0000;
+
+  digits[6][7] = B0000;
+  digits[6][6] = B0110;
+  digits[6][5] = B1000;
+  digits[6][4] = B1110;
+  digits[6][3] = B1001;
+  digits[6][2] = B1001;
+  digits[6][1] = B0110;
+  digits[6][0] = B0000;
+
+  digits[7][7] = B0000;
+  digits[7][6] = B1111;
+  digits[7][5] = B0001;
+  digits[7][4] = B0001;
+  digits[7][3] = B0010;
+  digits[7][2] = B0100;
+  digits[7][1] = B1000;
+  digits[7][0] = B0000;
 }
 
 void clearScreen() {
@@ -27,9 +103,28 @@ void clearScreen() {
   }
 }
 
+void setText(char* text, byte len) {
+  for (int i = 0; i < 32; i++) {
+    for (int y = 0; y < 8; y++) {
+      displayContent[i][y] = 0;
+    }
+  }
+  for (int i = 0; i < len; i++) {
+    byte *digit = digits[text[i] - 0x30];
+    byte firstSegment = (i * 5) / 8;
+    byte secondSegment = (i * 5 + 5) / 8;
+    byte shiftLeft = 8 - ((i * 5 + 4) % 8);
+    byte shiftRight = 8 - shiftLeft;
+    
+    for (int y = 0; y < 8; y++) {
+      displayContent[y][firstSegment] = displayContent[y][firstSegment] | (digit[y] >> shiftRight);
+      displayContent[y][secondSegment] = displayContent[y][secondSegment] | (digit[y] << shiftLeft);
+    }
+  }
+}
+
 void updateLED() {
-  if(millis() - lastUpdate > 1000) {
-    currentOffset = currentOffset + 1 % displayLength;
+  if(millis() - lastUpdate > 100) {
     for (byte y = 0; y < 8; y++) {
       byte addr = y + 1;
       byte addresses[4] = {addr, addr, addr, addr};
@@ -39,13 +134,15 @@ void updateLED() {
           byte x = (currentOffset + i + module * 8) % displayLength;
           byte segment = x / 8;
           byte shiftRight = x % 8;
-          byte shifted = displayContent[y][segment] >> shiftRight;
+          byte shifted = displayContent[y][segment] >> (7 - shiftRight);
           byte masked = shifted & 0x01;
           values[3 - module] = values[3 - module] | (masked << i);
         }
       }
       sendData(addresses, values);
     }
+    currentOffset = (currentOffset + 1) % displayLength;
+    lastUpdate = millis();
   }
 }
 
